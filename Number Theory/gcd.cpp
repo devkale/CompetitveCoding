cd4420413dfe903395273eed68a4dce93a3c147d
devkale <kaleaditya92@gmail.com>
2016-05-27 20:56:48 +0530

    number theory gcd

---
+++ Number Theory/gcd.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+int gcd(int a, int b)
+{
+	int r;
+	while(b!=0)
+	{
+		r = a%b;
+		a = b;
+		b  = r;
+	}
+	return a;
+}
+int main()
+{
+	int a,b;
+	cin>>a>>b;
+	int answer = gcd(a,b);
+	cout<<answer<<endl;
+	return 0;
+}
